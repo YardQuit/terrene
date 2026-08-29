5cc801c10c6c8f4811be4b70e08283e8ccb880bd
Michael Jones <yardquit@pm.me>
2026-08-29 12:36:28 +0200

    Bump donkey.el to version 1.4.1

--- build_files/build.sh
+++ build_files/build.sh
@@ -56,8 +56,8 @@ chmod -R +x /etc/cron.daily 2>/dev/null || true
 ##   COMMIT=$(git ls-remote https://github.com/YardQuit/donkey master | awk '{print $1}')
 ##   echo $COMMIT; curl -fsSL https://raw.githubusercontent.com/YardQuit/donkey/$COMMIT/donkey.el | sha256sum
 
-DONKEY_COMMIT="c4a61bde229420a71f30733664ad6b3ac6c336e5"   # 1.4.0
-DONKEY_SHA256="3e07b21c2d94b57908e332cb200d773ca0ce4d3ea2a9623e18096e4fcb895fb5"
+DONKEY_COMMIT="32bcffe448e35622c52a0738d43d56b6e84d0886 "   # 1.4.1
+DONKEY_SHA256="794c10a76b89f9ced6713c30d485c76fdf54fff1a1c9462bdbeb8a11795dae83"
 
 ## The directory is created explicitly: curl's --create-dirs would make it
 ## 0750, and /etc/skel content must be world-readable or copying it by hand
